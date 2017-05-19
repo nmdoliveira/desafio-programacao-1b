86f9edee911e22c7e464900cb2e5bac5d9165377
Nathalia Oliveira <nathipsl@hotmail.com>
2017-05-19 13:32:54 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+
+gem "rails", "~> 4.0"
+
+gem "sqlite3"
+
+gem "sass-rails"
+
+group :test do
+  gem "rspec-rails"
+end
+
+group :development do
+  gem "rubocop"
+end
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require File.expand_path("../boot", __FILE__)
+
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module DesafioProgramacao1b
+  class Application < Rails::Application
+    config.time_zone = "Brasilia"
+
+    config.i18n.available_locales = [:"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
